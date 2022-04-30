2c4c7f29fb8bbefd5c689ba652f9c3374ef4808d
hxia999 <65902824+hxia999@users.noreply.github.com>
2022-04-30 11:47:34 +0800

    update

---
+++ BlazorBarcodeToolkit/Enums/BarcodeEncoding.cs
@@ -0,0 +1,35 @@
+﻿
+public enum BarcodeEncoding
+{
+    Cp437,
+    ISO_8859_1,
+    ISO_8859_2,
+    ISO_8859_3,
+    ISO_8859_4,
+    ISO_8859_5,
+    ISO_8859_6,
+    ISO_8859_7,
+    ISO_8859_8,
+    ISO_8859_9,
+    ISO_8859_10,
+    ISO_8859_11,
+    ISO_8859_13,
+    ISO_8859_14,
+    ISO_8859_15,
+    ISO_8859_16,
+    Shift_JIS,
+    windows_1250,
+    windows_1251,
+    windows_1252,
+    windows_1256,
+    UTF_16BE,
+    UTF_8,
+    ASCII,
+    Big5,
+    GB2312,
+    GB18030,
+    EUC_CN,
+    GBK,
+    EUC_KR
+
+}
---
+++ BlazorBarcodeToolkit/Enums/BarcodeFormat.cs
@@ -0,0 +1,17 @@
+﻿public enum BarcodeFormat
+{
+    AZTEC,
+    CODABAR,
+    CODE_39,
+    CODE_93,
+    CODE_128,
+    DATA_MATRIX,
+    EAN_8,
+    EAN_13,
+    ITF,
+    PDF_417,
+    QR_CODE,
+    UPC_A,
+    UPC_E,
+
+}
